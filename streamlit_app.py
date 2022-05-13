4a9836943f8cd99efb3f1c9f740983e84a425b01
Petr Lindovsky <105349859+petrlindovsky@users.noreply.github.com>
2022-05-13 16:12:45 +0200

    Update streamlit_app.py
    added end of file comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,3 +80,5 @@ streamlit.write('Thanks for adding', add_my_fruit)
 # This will no twork correctly, but just go with it for now.
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 #
+
+# And this is end of file
