828e5f83a77d3b9d0ec57d15d6d8d026e843a31f
Petr Lindovsky <105349859+petrlindovsky@users.noreply.github.com>
2022-05-13 15:34:26 +0200

    Update streamlit_app.py
    new branch before further changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,3 +69,4 @@ streamlit.write('Thanks for adding', add_my_fruit)
 
 # This will no twork correctly, but just go with it for now.
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#
